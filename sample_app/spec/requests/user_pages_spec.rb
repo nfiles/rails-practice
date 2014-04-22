fe7f5d2df73bdd92a7007eba525f03ce78b2f9e1
Nathan Files <nathan@nfiles-ubusrv.(none)>
2014-04-22 12:05:50 -0400

    Testing/Debug - debug info in development environment - users resource - users routing - test user (with FactoryGirl factory)

--- sample_app/spec/requests/user_pages_spec.rb
+++ sample_app/spec/requests/user_pages_spec.rb
@@ -4,10 +4,19 @@ describe "User Pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
   describe "Signup Page" do
     before { visit signup_path }
 
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
+
 end
