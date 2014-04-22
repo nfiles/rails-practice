1fb24abddd67e2249d1b29f2c0cdf0293888362a
Nathan Files <nathan@nfiles-ubusrv.(none)>
2014-04-22 10:57:09 -0400

    Explicitly name routes in routes.rb

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
-  match '/signup',  to: 'users#new',            via: 'get'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',  to: 'users#new',            via: 'get', as: 'signup'
+  match '/help',    to: 'static_pages#help',    via: 'get', as: 'help'
+  match '/about',   to: 'static_pages#about',   via: 'get', as: 'about'
+  match '/contact', to: 'static_pages#contact', via: 'get', as: 'contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
