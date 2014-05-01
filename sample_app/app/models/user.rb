7064ca95eb86509270a4164b1a8f7624a7007302
Nathan Files <nfiles2009@gmail.com>
2014-05-01 16:20:48 -0400

    Micropost feed - on home page, list all microposts from logged-in user

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -27,6 +27,11 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # This is preliminary. See "Following Users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
   
   def create_remember_token
